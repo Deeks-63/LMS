620015c5560a3446d4dff226fef14a1e310769bb
Deekshant Goyal <deekshantgoyal63@gmail.com>
2022-12-30 21:53:41 +0530

    Add files via upload

---
+++ projfront/src/UserDashboard/Widgets/SmallCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, CardHeader, Typography, CardMedia } from "@mui/material"
+
+function SmallCard(props) {
+    return ( < >
+        <
+        h1 > Welcome to the Dashboard User < /h1> <
+        />
+    )
+}
+
+export default SmallCard
